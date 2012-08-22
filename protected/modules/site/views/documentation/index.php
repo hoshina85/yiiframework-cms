167bd2c95315a8c4886e5ed1f894a3e0d547a7d2
hoshina85 <hoshina85@gmail.com>
2012-08-22 11:25:48 +0900

    document index

--- protected/modules/site/views/documentation/index.php
+++ protected/modules/site/views/documentation/index.php
@@ -1,1 +1,9 @@
-Documentation
+<div id='custompagecontent'>
+<h2>ドキュメント</h2>
+<p>以下に掲載するドキュメントは公式のドキュメントと同様のものです。</p>
+<ul>
+<li> <?php echo CHtml::link('Yiiの公式ガイド',array('documentation/guide'))?>
+<li> <?php echo CHtml::link('Yiiを使ってブログシステムを作る',array('documentation/blog'))?>
+</ul>
+</div>
+
